6740bb33eeb6bf485fb5cbb256e2889198a463af
Alessandro Casazza <alessandro@commercelayer.io>
2020-02-06 19:24:40 +0100

    test(core): updated

---
+++ cypress/integration/price.spec.ts
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+import React from 'react'
+import PriceContainer from '../../src/components/PriceContainer'
+import Price from '../../src/components/Price'
+
+describe('PriceContainer and Price component', () => {
+  it('', () => {})
+})
