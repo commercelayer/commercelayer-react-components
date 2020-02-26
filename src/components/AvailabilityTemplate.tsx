7128946395d8f50e3c1136100f86e5f11fea94ba
Alessandro Casazza <alessandro@commercelayer.io>
2020-02-26 18:07:07 +0100

    Alessandro - fix(core): showShippingMethodName prop error

--- src/components/AvailabilityTemplate.tsx
+++ src/components/AvailabilityTemplate.tsx
@@ -3,10 +3,13 @@ import AvailabilityContext from '../context/AvailabilityContext'
 import Parent from './utils/Parent'
 import { BaseComponent } from '../@types/index'
 import _ from 'lodash'
+import PropTypes from 'prop-types'
+
+type TimeFormat = 'days' | 'hours'
 
 export interface AvailabilityTemplateProps extends BaseComponent {
   children?: FunctionComponent
-  timeFormat?: 'days' | 'hours'
+  timeFormat?: TimeFormat
   showShippingMethodName?: boolean
 }
 
@@ -15,7 +18,10 @@ const AvailabilityTemplate: FunctionComponent<AvailabilityTemplateProps> = props
   const { min, max, shippingMethod } = useContext(AvailabilityContext)
   const mn = !_.isEmpty(min) ? min[timeFormat] : ''
   const mx = !_.isEmpty(max) ? max[timeFormat] : ''
-  const name = showShippingMethodName ? `with ${shippingMethod.name}` : ''
+  const name =
+    showShippingMethodName && shippingMethod
+      ? `with ${shippingMethod.name}`
+      : ''
   const text =
     mn && mx ? `Available in ${mn} - ${mx} ${timeFormat} ${name}` : ''
   const parentProps = {
@@ -34,6 +40,12 @@ const AvailabilityTemplate: FunctionComponent<AvailabilityTemplateProps> = props
 AvailabilityTemplate.defaultProps = {
   timeFormat: 'days',
   showShippingMethodName: false
+}
+
+AvailabilityTemplate.propTypes = {
+  timeFormat: PropTypes.oneOf<TimeFormat>(['days', 'hours']),
+  showShippingMethodName: PropTypes.bool,
+  children: PropTypes.func
 }
 
 export default AvailabilityTemplate
