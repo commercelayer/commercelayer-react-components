39195fb3a6e498678fdee00be1430772571e52cc
Alessandro Casazza <alessandro@commercelayer.io>
2020-04-03 19:09:04 +0200

    Alessandro - feat(LineItemOption): added new tests

---
+++ specs/LineItemOption.spec.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { LineItemOption } from '../src'
+import renderer from 'react-test-renderer'
+import components from '../src/config/components'
+import Parent from '../src/components/utils/Parent'
+
+const propTypes = components.LineItemOption.propTypes
+
+test('<LineItemOption/>', () => {
+  expect.assertions(6)
+  const component = renderer.create(<LineItemOption name="test" />)
+  const tree = component.toJSON()
+  const root = component.toTree()
+  const proptypes = root.type['propTypes']
+  expect(tree).toMatchSnapshot()
+  expect(proptypes.children).toBe(propTypes.children)
+  expect(proptypes.name).toBe(propTypes.name)
+  expect(proptypes.optionKeyClassName).toBe(propTypes.optionKeyClassName)
+  expect(proptypes.optionKeyId).toBe(propTypes.optionKeyId)
+  expect(proptypes.optionKeyStyle).toBe(propTypes.optionKeyStyle)
+})
+
+test('<LineItemOption proptypes required />', () => {
+  expect.assertions(2)
+  console.error = jest.fn()
+  const component = renderer.create(<LineItemOption />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+  expect(console.error.mock.calls[0][0]).toEqual(
+    expect.stringContaining(
+      'Warning: Failed prop type: The prop `name` is marked as required in `LineItemOption`, but its value is `undefined`.'
+    )
+  )
+})
+
+test('<LineItemOption with custom children />', () => {
+  // expect.assertions(7)
+  const CustomComponent = (props) => <span>{props.label}</span>
+  const component = renderer.create(
+    <LineItemOption name="test">{CustomComponent}</LineItemOption>
+  )
+  const tree = component.toJSON()
+  const root = component.toTree()
+  const rendered = root.rendered
+  const childRendered = root.rendered.rendered
+  expect(tree).toMatchSnapshot()
+  expect(rendered.type).toBe(Parent)
+  expect(rendered.props.children).toBe(CustomComponent)
+  expect(childRendered.nodeType).toBe('component')
+  expect(childRendered.props.name).toBe('test')
+  expect(childRendered.type).toBe(CustomComponent)
+  expect(childRendered.rendered.type).toBe('span')
+})
--- src/components/LineItemOption.tsx
+++ src/components/LineItemOption.tsx
@@ -1,22 +1,17 @@
 import React, { FunctionComponent, useContext, Fragment } from 'react'
-import PropTypes, { InferProps } from 'prop-types'
-import { BC } from '../@types'
 import LineItemOptionChildrenContext from '../context/LineItemOptionChildrenContext'
 import _ from 'lodash'
 import Parent from './utils/Parent'
+import { PropsType } from '../utils/PropsType'
+import components from '../config/components'
 
-const LIOProps = {
-  ...BC,
-  name: PropTypes.string.isRequired,
-  children: PropTypes.func,
-  optionKeyClassName: PropTypes.string,
-  optionKeyId: PropTypes.string,
-  optionKeyStyle: PropTypes.object
-}
+const propTypes = components.LineItemOption.propTypes
+const displayName = components.LineItemOption.displayName
 
-export type LineItemOptionProps = InferProps<typeof LIOProps>
+export type LineItemOptionProps = PropsType<typeof propTypes> &
+  JSX.IntrinsicElements['span']
 
-const LineItemOption: FunctionComponent<LineItemOptionProps> = props => {
+const LineItemOption: FunctionComponent<LineItemOptionProps> = (props) => {
   const {
     name,
     children,
@@ -31,28 +26,28 @@ const LineItemOption: FunctionComponent<LineItemOptionProps> = props => {
   const { lineItemOption } = useContext(LineItemOptionChildrenContext)
   const parentProps = {
     ...props,
-    lineItemOption
+    lineItemOption,
   }
   return children ? (
     <Parent {...parentProps}>{props.children}</Parent>
   ) : _.has(lineItemOption, `options.${name}`) ? (
     <Fragment>
       <span
-        id={optionKeyId}
-        style={optionKeyStyle}
-        className={optionKeyClassName}
+        id={optionKeyId || ''}
+        style={optionKeyStyle || {}}
+        className={optionKeyClassName || ''}
         {...p}
-      >{`${name}:`}</span>
-      <span
-        id={id}
-        style={style}
-        className={className}
-        {...p}
-      >{`${lineItemOption.options[name]}`}</span>
+      >
+        {`${name}:`}
+      </span>
+      <span id={id} style={style} className={className} {...p}>
+        {`${lineItemOption['options'][name]}`}
+      </span>
     </Fragment>
   ) : null
 }
 
-LineItemOption.propTypes = LIOProps
+LineItemOption.propTypes = propTypes
+LineItemOption.displayName = displayName
 
 export default LineItemOption
