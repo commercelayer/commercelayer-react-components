95276733508feaa65af142085f37963051bbc54f
Alessandro Casazza <alessandro@commercelayer.io>
2020-04-09 19:53:43 +0200

    Alessandro - core(SkuOptionInput): added tests, new config and fixed null variables

--- src/components/SkuOptionInput.tsx
+++ src/components/SkuOptionInput.tsx
@@ -2,40 +2,49 @@ import React, { FunctionComponent, useContext, ChangeEvent } from 'react'
 import BaseInput from './utils/BaseInput'
 import ItemContext from '../context/ItemContext'
 import SkuOptionChildrenContext from '../context/SkuOptionChildrenContext'
-import { BOCProps } from './utils/BaseOrderPrice'
-import { BaseInputProps } from './utils/BaseInput'
 import _ from 'lodash'
+import { ItemOptions } from '../reducers/ItemReducer'
+import { PropsType } from '../utils/PropsType'
+import components from '../config/components'
+
+const propTypes = components.SkuOptionInput.propTypes
+const displayName = components.SkuOptionInput.displayName
 
 type HandleChange = (
   event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
 ) => void
 
-const SkuOptionInput: FunctionComponent<BaseInputProps> = props => {
+type SkuOptionInputProps = PropsType<typeof propTypes> &
+  JSX.IntrinsicElements['input'] &
+  JSX.IntrinsicElements['textarea']
+
+const SkuOptionInput: FunctionComponent<SkuOptionInputProps> = (props) => {
   const { name } = props
   const { option, setOption } = useContext(ItemContext)
   const { skuOption, skuCode } = useContext(SkuOptionChildrenContext)
-  const handleChange: HandleChange = e => {
+  const handleChange: HandleChange = (e) => {
     const val = e.target.value
-    const options = _.has(option, `${skuCode}.${skuOption.id}`)
-      ? option[skuCode][skuOption.id]['options']
+    const options = _.has(option, `${skuCode}.${skuOption?.id}`)
+      ? option[skuCode][`${skuOption?.id}`]['options']
       : {}
     const o = {
       [skuCode]: {
         ...option[skuCode],
-        [skuOption.id]: {
-          skuOptionId: skuOption.id,
+        [`${skuOption?.id}`]: {
+          skuOptionId: skuOption?.id,
           options: {
             ...options,
-            [name]: val
-          }
-        }
-      }
-    }
-    setOption(o)
+            [name]: val,
+          },
+        },
+      },
+    } as ItemOptions
+    setOption && setOption(o)
   }
   return <BaseInput onChange={handleChange} {...props} />
 }
 
-SkuOptionInput.propTypes = BOCProps
+SkuOptionInput.propTypes = propTypes
+SkuOptionInput.displayName = displayName
 
 export default SkuOptionInput
