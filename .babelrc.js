c09cf0581a250ce3645798bbaf1ed4e6861b94a9
Alessandro Casazza <alessandro@commercelayer.io>
2020-05-08 18:28:40 +0200

    Alessandro - chore(deps): updated

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,11 @@
-let presets = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+const config = {
+  presets: ['next/babel'],
 }
-if (process.env.npm_lifecycle_script.search('tsdx build') === -1) {
-  presets = {
-    presets: ['next/babel'],
-  }
+if (
+  process.env.npm_lifecycle_script &&
+  process.env.npm_lifecycle_script.search('tsdx build') !== -1
+) {
+  config.presets = ['@babel/preset-env', '@babel/preset-react']
+  console.log('build config 🚀', config)
 }
-module.exports = presets
+module.exports = config
