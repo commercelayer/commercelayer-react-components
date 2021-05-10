ce6837aaf571fe046e43e64a310a1ceb1f626254
Alessandro Casazza <alessandro@commercelayer.io>
2021-05-10 12:38:52 +0200

    fix: React useEffect warnings

--- src/components/ShippingAddressContainer.tsx
+++ src/components/ShippingAddressContainer.tsx
@@ -38,6 +38,7 @@ const ShippingAddressContainer: FunctionComponent<Props> = (props) => {
         setCloneAddress,
       })
     }
+    return () => {}
   }, [order])
   const contextValue = {
     ...state,
