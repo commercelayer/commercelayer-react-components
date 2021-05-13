ca8d162af48ef6dbaa27e36ffe078f988c2075ff
Alessandro Casazza <alessandro@commercelayer.io>
2021-05-13 17:09:34 +0200

    chore: Update config and scripts

--- .babelrc.js
+++ .babelrc.js
@@ -7,8 +7,9 @@ const config = {
   },
 }
 if (
-  process.env.npm_lifecycle_script &&
-  process.env.npm_lifecycle_script.search('tsdx build') !== -1
+  (process.env.npm_lifecycle_script &&
+    process.env.npm_lifecycle_script.search('tsdx build') !== -1) ||
+  process.env.npm_lifecycle_script.search('tsdx watch') !== -1
 ) {
   config.presets = ['@babel/preset-env', '@babel/preset-react']
   console.log('build config 🚀', config)
