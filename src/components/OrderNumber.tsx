02163211a748f435924b9cc5b852e602a648c2af
Alessandro Casazza <alessandro@commercelayer.io>
2021-06-29 16:38:10 +0200

    feat: Add OrderNumber component

---
+++ src/components/OrderNumber.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react'
+import components from '#config/components'
+import BaseField, { BaseFieldProps } from './utils/BaseField'
+
+const propTypes = components.OrderNumber.propTypes
+const displayName = components.OrderNumber.displayName
+
+export type OrderNumberProps = Omit<BaseFieldProps, 'attribute'>
+
+const OrderNumber: FunctionComponent<OrderNumberProps> = (props) => {
+  return <BaseField attribute="number" {...props} />
+}
+
+OrderNumber.propTypes = propTypes
+OrderNumber.displayName = displayName
+
+export default OrderNumber
---
+++ src/components/utils/BaseField.tsx
@@ -0,0 +1,39 @@
+import OrderContext from '#context/OrderContext'
+import React, {
+  FunctionComponent,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import Parent from './Parent'
+
+type BaseFieldChildren = Omit<BaseFieldProps, 'children'>
+
+export type BaseFieldProps = {
+  attribute: 'number' | 'id'
+  children?: (props: BaseFieldChildren) => ReactNode
+} & JSX.IntrinsicElements['span']
+
+const BaseField: FunctionComponent<BaseFieldProps> = ({
+  children,
+  attribute,
+  ...p
+}) => {
+  const { order } = useContext(OrderContext)
+  const [field, setField] = useState('')
+  useEffect(() => {
+    if (order && attribute in order) setField(order[attribute] as string)
+    return () => {
+      setField('')
+    }
+  }, [order])
+  const parentProps = { attribute: field, ...p }
+  return children ? (
+    <Parent {...parentProps}>{children}</Parent>
+  ) : (
+    <span {...p}>{field}</span>
+  )
+}
+
+export default BaseField
