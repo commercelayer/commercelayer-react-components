ab503e88bd3ba9e5f26e05f1a5b10ebbbe4fe51f
Alessandro Casazza <alessandro@commercelayer.io>
2021-07-09 19:13:58 +0200

    feat: Add props for actions column

--- src/context/OrderListChildrenContext.ts
+++ src/context/OrderListChildrenContext.ts
@@ -1,14 +1,19 @@
-import { createContext } from 'react'
+import OrderAttributes from '#typings/order'
+import { createContext, ReactNode } from 'react'
 import { Row } from 'react-table'
 
 export interface InitialOrderListContext {
-  order: Record<string, any>
-  row: Row
+  order: OrderAttributes | null
+  row: Row<OrderAttributes>
+  showActions: boolean
+  actionsComponent?: (props: { order: OrderAttributes }) => ReactNode
+  actionsContainerClassName?: string
 }
 
 const initial: InitialOrderListContext = {
-  order: {},
+  order: null,
   row: {} as any,
+  showActions: false,
 }
 
 const OrderListChildrenContext = createContext<InitialOrderListContext>(initial)
