e78aec18cbaff7edb318b6ed9604027daa721858
Alessandro Casazza <alessandro@commercelayer.io>
2021-08-03 18:10:54 +0200

    fix: Click the payment container

--- src/context/PaymentMethodChildrenContext.ts
+++ src/context/PaymentMethodChildrenContext.ts
@@ -3,12 +3,13 @@ import { PaymentMethodCollection } from '@commercelayer/js-sdk'
 
 export interface InitialPaymentMethodChildrenContext {
   payment?: PaymentMethodCollection
+  clickableContainer?: boolean
+  paymentSelected?: string
 }
 
 const initial: InitialPaymentMethodChildrenContext = {}
 
-const PaymentMethodChildrenContext = createContext<InitialPaymentMethodChildrenContext>(
-  initial
-)
+const PaymentMethodChildrenContext =
+  createContext<InitialPaymentMethodChildrenContext>(initial)
 
 export default PaymentMethodChildrenContext
