820b93d136b71d734c6cefa2e022f21b5bd25617
Alessandro Casazza <alessandro@commercelayer.io>
2021-09-24 16:53:50 +0200

    chore: Move icons to utils folder

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -28,39 +28,6 @@ import { sortDescIcon, sortAscIcon } from '#utils/icons'
 
 const propTypes = components.OrderList.propTypes
 const displayName = components.OrderList.displayName
-const sortDescIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={20}
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
-    />
-  </svg>
-)
-
-const sortAscIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={20}
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
-    />
-  </svg>
-)
 
 type OrderListProps = {
   children: ReactNode
