a3b61a17d7ed01e76c43ae39bd0918f1f0ad1333
Alessandro Casazza <alessandro@commercelayer.io>
2021-10-07 16:15:01 +0200

    feat: Add CRUD functions for Customer Address

--- src/components/customers/CustomerAddressForm.tsx
+++ src/components/customers/CustomerAddressForm.tsx
@@ -1,24 +1,32 @@
 import AddressesContext from '#context/AddressContext'
-import { useRapidForm } from 'rapid-form'
-import { ReactNode, useContext, useEffect, useRef } from 'react'
+import useRapidForm from 'rapid-form'
+import React, {
+  FunctionComponent,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+} from 'react'
 import CustomerAddressFormContext from '#context/CustomerAddressFormContext'
+import { isEmpty } from 'lodash'
 import { BaseError, CodeErrorType } from '#typings/errors'
 import { AddressField } from '#reducers/AddressReducer'
 import { AddressCountrySelectName, AddressInputName } from '#typings'
 import components from '#config/components'
 import OrderContext from '#context/OrderContext'
 import isEmptyStates from '#utils/isEmptyStates'
-import type { Address } from '@commercelayer/sdk'
 
 const propTypes = components.CustomerAddressForm.propTypes
 
-type Props = {
+type CustomerAddressFormProps = {
   children: ReactNode
   reset?: boolean
   errorClassName?: string
 } & Omit<JSX.IntrinsicElements['form'], 'onSubmit'>
 
-export function CustomerAddressForm(props: Props) {
+const CustomerAddressForm: FunctionComponent<CustomerAddressFormProps> = (
+  props
+) => {
   const {
     children,
     errorClassName,
@@ -26,26 +34,23 @@ export function CustomerAddressForm(props: Props) {
     reset = false,
     ...p
   } = props
-  console.log(useRapidForm)
-  debugger
   const { validation, values, errors, reset: resetForm } = useRapidForm()
   const { setAddressErrors, setAddress } = useContext(AddressesContext)
   const { order } = useContext(OrderContext)
-  const ref = useRef<HTMLFormElement | null>(null)
+  const ref = useRef<HTMLFormElement>(null)
   useEffect(() => {
-    if (Object.keys(errors).length > 0) {
+    if (!isEmpty(errors)) {
       const formErrors: BaseError[] = []
       for (const fieldName in errors) {
-        const code = errors[fieldName]?.code
-        const message = errors[fieldName]?.message || ''
-        if (fieldName === 'billing_address_state_code') {
-          if (values['state_code']) {
+        const { code, message } = errors[fieldName]
+        if (['customer_address_state_code'].includes(fieldName)) {
+          if (isEmpty(values['state_code'])) {
             delete errors[fieldName]
           } else {
             formErrors.push({
               code: code as CodeErrorType,
               message,
-              resource: 'billing_address',
+              resource: 'customerAddress',
               field: fieldName,
             })
           }
@@ -53,41 +58,37 @@ export function CustomerAddressForm(props: Props) {
           formErrors.push({
             code: code as CodeErrorType,
             message,
-            resource: 'billing_address',
+            resource: 'customerAddress',
             field: fieldName,
           })
         }
       }
-      setAddressErrors(formErrors, 'billing_address')
-    } else if (Object.keys(values).length > 0) {
-      setAddressErrors([], 'billing_address')
+      setAddressErrors(formErrors, 'customerAddress')
+    } else if (!isEmpty(values)) {
+      setAddressErrors([], 'customerAddress')
       for (const name in values) {
         const field = values[name]
         if (field?.value) {
-          values[name.replace('billing_address_', '')] = field.value
+          values[name.replace('customer_address_', '')] = field.value
           delete values[name]
         }
-        if (['billing_address_state_code'].includes(name)) {
-          const countryCode = (values['billing_address_country_code']?.[
-            'value'
-          ] || values['country_code']) as string
+        if (['customer_address_state_code'].includes(name)) {
+          const countryCode =
+            values['customer_address_country_code']?.value ||
+            values['country_code']
           if (!isEmptyStates(countryCode) && !field.value) {
-            delete values['billing_address_state_code']
+            delete values['customer_address_state_code']
           }
         }
       }
-      setAddress({ values: values as Address, resource: 'billing_address' })
+      setAddress({ values, resource: 'customerAddress' })
     }
-    if (
-      reset &&
-      (Object.keys(values).length > 0 || Object.keys(errors).length > 0)
-    ) {
+    if (reset && (!isEmpty(values) || !isEmpty(errors))) {
       if (ref) {
         ref.current?.reset()
-        // @ts-ignore
-        resetForm({ target: ref.current })
-        setAddressErrors([], 'billing_address')
-        setAddress({ values: {} as Address, resource: 'billing_address' })
+        resetForm({ target: ref.current } as any)
+        setAddressErrors([], 'customerAddress')
+        setAddress({ values: {}, resource: 'customerAddress' })
       }
     }
   }, [errors, values, reset])
@@ -95,13 +96,10 @@ export function CustomerAddressForm(props: Props) {
     name: AddressField | AddressInputName | AddressCountrySelectName,
     value: any
   ) => {
-    const field = {
-      [name.replace('billing_address_', '')]: value,
+    const field: any = {
+      [name.replace('customer_address_', '')]: value,
     }
-    setAddress({
-      values: { ...(values as Address), ...field },
-      resource: 'billing_address',
-    })
+    setAddress({ values: { ...values, ...field }, resource: 'customerAddress' })
   }
   const providerValues = {
     values,
@@ -112,7 +110,6 @@ export function CustomerAddressForm(props: Props) {
     requiresBillingInfo: order?.requiresBillingInfo || false,
     errors: errors as any,
     resetField: (name: string) =>
-      // @ts-ignore
       resetForm({ currentTarget: ref.current } as any, name),
   }
   return (
