4f1278d76deba6e87b3b16c83c713bff7485e686
Alessandro Casazza <alessandro@commercelayer.io>
2021-12-02 16:44:19 +0100

    chore: Remove OrderStorage

--- pages/order-summary.tsx
+++ pages/order-summary.tsx
@@ -53,104 +53,103 @@ export default function Order() {
       <Nav links={['/multiOrder', '/multiApp', '/giftCard']} />
       <CommerceLayer accessToken={token} endpoint={endpoint}>
         <div className="container mx-auto mt-5 px-5">
-          <OrderStorage persistKey="orderUS">
-            <OrderContainer orderId={orderId}>
-              <h1 className="text-4xl border-b-2 my-5">
-                Order Summary n - <OrderNumber />
-              </h1>
-              <LineItemsContainer>
-                <p className="text-sm m-2">
-                  Your shopping bag contains{' '}
-                  <LineItemsCount id="items-count" className="font-bold" />{' '}
-                  items
-                </p>
-                <div className="flex flex-col p-2">
-                  <LineItem>
-                    <div className="flex justify-around items-center border-b p-5">
-                      <LineItemImage className="p-2" width={80} />
-                      <LineItemName id="line-item-name" className="p-2" />
-                      <LineItemQuantity
-                        id="line-item-quantity"
-                        max={100}
-                        className="p-2"
-                        readonly
-                      />
-                      <LineItemAmount id="line-item-total" className="p-2" />
-                    </div>
-                  </LineItem>
-                  <LineItem type="gift_cards">
-                    <div className="flex justify-between items-center border-b p-5">
-                      <LineItemImage className="p-2" width={40} />
-                      <LineItemName id="line-item-name" className="p-2" />
-                      <LineItemQuantity
-                        id="line-item-quantity"
-                        max={10}
-                        className="p-2"
-                        readonly
-                      />
-                      <LineItemAmount id="line-item-total" className="p-2" />
-                    </div>
-                  </LineItem>
+          {/* <OrderStorage persistKey="orderUS"> */}
+          <OrderContainer orderId={orderId}>
+            <h1 className="text-4xl border-b-2 my-5">
+              Order Summary n - <OrderNumber />
+            </h1>
+            <LineItemsContainer>
+              <p className="text-sm m-2">
+                Your shopping bag contains{' '}
+                <LineItemsCount id="items-count" className="font-bold" /> items
+              </p>
+              <div className="flex flex-col p-2">
+                <LineItem>
+                  <div className="flex justify-around items-center border-b p-5">
+                    <LineItemImage className="p-2" width={80} />
+                    <LineItemName id="line-item-name" className="p-2" />
+                    <LineItemQuantity
+                      id="line-item-quantity"
+                      max={100}
+                      className="p-2"
+                      readonly
+                    />
+                    <LineItemAmount id="line-item-total" className="p-2" />
+                  </div>
+                </LineItem>
+                <LineItem type="gift_cards">
+                  <div className="flex justify-between items-center border-b p-5">
+                    <LineItemImage className="p-2" width={40} />
+                    <LineItemName id="line-item-name" className="p-2" />
+                    <LineItemQuantity
+                      id="line-item-quantity"
+                      max={10}
+                      className="p-2"
+                      readonly
+                    />
+                    <LineItemAmount id="line-item-total" className="p-2" />
+                  </div>
+                </LineItem>
+              </div>
+            </LineItemsContainer>
+            <div className="flex flex-col w-1/2 m-auto">
+              <div className="flex items-center p-2 justify-around font-medium text-left">
+                <div className="w-full">
+                  <p className="text-lg">Subtotal </p>
                 </div>
-              </LineItemsContainer>
-              <div className="flex flex-col w-1/2 m-auto">
-                <div className="flex items-center p-2 justify-around font-medium text-left">
-                  <div className="w-full">
-                    <p className="text-lg">Subtotal </p>
-                  </div>
-                  <div className="text-right">
-                    <SubTotalAmount />
-                  </div>
-                </div>
-                <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
-                  <div className="w-full">
-                    <p className="text-lg">Discount </p>
-                  </div>
-                  <div className="text-right">
-                    <DiscountAmount>
-                      {(props) => {
-                        return null
-                      }}
-                    </DiscountAmount>
-                  </div>
-                </div>
-                <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
-                  <div className="w-full">
-                    <p className="text-lg">Shipping </p>
-                  </div>
-                  <div className="text-right">
-                    <ShippingAmount />
-                  </div>
-                </div>
-                <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
-                  <div className="w-full">
-                    <p className="text-lg">
-                      Taxes <span className="text-sm font-tin">(included)</span>
-                    </p>
-                  </div>
-                  <div className="text-right">
-                    <TaxesAmount />
-                  </div>
-                </div>
-                <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
-                  <div className="w-full">
-                    <p className="text-lg">Gift card </p>
-                  </div>
-                  <div className="text-right">
-                    <GiftCardAmount />
-                  </div>
-                </div>
-                <div className=" flex items-center p-2 justify-around font-bold text-left">
-                  <div className="w-full">
-                    <p className="text-lg mr-2">Total </p>
-                  </div>
-                  <div className="text-right">
-                    <TotalAmount id="total-amount" />
-                  </div>
+                <div className="text-right">
+                  <SubTotalAmount />
                 </div>
               </div>
-            </OrderContainer>
-          </OrderStorage>
+              <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
+                <div className="w-full">
+                  <p className="text-lg">Discount </p>
+                </div>
+                <div className="text-right">
+                  <DiscountAmount>
+                    {(props) => {
+                      return null
+                    }}
+                  </DiscountAmount>
+                </div>
+              </div>
+              <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
+                <div className="w-full">
+                  <p className="text-lg">Shipping </p>
+                </div>
+                <div className="text-right">
+                  <ShippingAmount />
+                </div>
+              </div>
+              <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
+                <div className="w-full">
+                  <p className="text-lg">
+                    Taxes <span className="text-sm font-tin">(included)</span>
+                  </p>
+                </div>
+                <div className="text-right">
+                  <TaxesAmount />
+                </div>
+              </div>
+              <div className=" flex items-center p-2 justify-around text-gray-600 text-left">
+                <div className="w-full">
+                  <p className="text-lg">Gift card </p>
+                </div>
+                <div className="text-right">
+                  <GiftCardAmount />
+                </div>
+              </div>
+              <div className=" flex items-center p-2 justify-around font-bold text-left">
+                <div className="w-full">
+                  <p className="text-lg mr-2">Total </p>
+                </div>
+                <div className="text-right">
+                  <TotalAmount id="total-amount" />
+                </div>
+              </div>
+            </div>
+          </OrderContainer>
+          {/* </OrderStorage> */}
         </div>
       </CommerceLayer>
     </Fragment>
