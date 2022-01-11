1c54a23f72285d31aa6d0b4ba55b52e279ede87f
Alessandro Casazza <alessandro@commercelayer.io>
2022-01-11 15:53:17 +0100

    chore: Update TailwindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-    },
   },
   plugins: [
     require('@tailwindcss/forms'),
