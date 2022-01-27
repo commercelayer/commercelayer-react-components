39b0db64d38ab8973f7b7fc66e04b723f36b8f52
Alessandro Casazza <alessandro@commercelayer.io>
2022-01-27 19:44:09 +0100

    fix: Reset form when isBusiness prop changes status

--- src/components/SaveAddressesButton.tsx
+++ src/components/SaveAddressesButton.tsx
@@ -44,7 +44,9 @@ export function SaveAddressesButton(props: Props) {
     saveAddresses,
     billingAddressId,
     shippingAddressId,
+    isBusiness,
   } = useContext(AddressContext)
+  console.log('isBusiness', isBusiness)
   const { order } = useContext(OrderContext)
   const { addresses, isGuest, createCustomerAddress } =
     useContext(CustomerContext)
@@ -75,6 +77,7 @@ export function SaveAddressesButton(props: Props) {
     shipping_address,
     shippingAddressId,
   })
+  debugger
   const disable =
     disabled ||
     customerEmail ||
