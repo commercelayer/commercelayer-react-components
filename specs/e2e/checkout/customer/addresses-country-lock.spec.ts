19e77847f5c8a7e455d8dbaf0e40cefcee1d2bd8
Alessandro Casazza <alessandro@commercelayer.io>
2022-01-27 12:12:58 +0100

    chore: Add specs for customer address country lock

---
+++ specs/e2e/checkout/customer/addresses-country-lock.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '../../baseFixtures'
+const endpoint = `checkout/customer/addresses-country-lock`
+import mock from '../../fixtures/checkout/customer/addresses-country-lock.json'
+import { getScreenshotPath } from '../../utils/response'
+
+test('Customer address country lock', async ({ page, browser, request }) => {
+  await page.route('**/oauth/token', (route) => {
+    route.fulfill({
+      status: 200,
+      headers: { 'access-control-allow-origin': '*' },
+      contentType: 'application/vnd.api+json',
+      body: JSON.stringify(mock.token),
+    })
+  })
+  await page.route('**/customer_addresses?include=address', (route) => {
+    route.fulfill({
+      status: 200,
+      headers: { 'access-control-allow-origin': '*' },
+      contentType: 'application/vnd.api+json',
+      body: JSON.stringify(mock.customer_addresses),
+    })
+  })
+  await page.route('**/orders/*', (route) => {
+    route.fulfill({
+      status: 200,
+      headers: { 'access-control-allow-origin': '*' },
+      contentType: 'application/vnd.api+json',
+      body: JSON.stringify(mock.orders),
+    })
+  })
+  await page.coverage.startJSCoverage()
+  await page.goto(endpoint)
+  const shipToDifferentAddressButton = await page.waitForSelector(
+    '[data-test=ship-to-different-address-button]'
+  )
+  await shipToDifferentAddressButton.click()
+  const shippingAddresses = await page.locator(
+    '[data-test=customer-shipping-address]'
+  )
+  const textContents = await shippingAddresses.allTextContents()
+  expect(textContents.length).toBe(2)
+  textContents.map((t) => {
+    expect(t).toContain('(IT)')
+  })
+  await page.screenshot({
+    path: getScreenshotPath('customer-addresses-country-lock.jpg'),
+  })
+  await page.coverage.stopJSCoverage()
+  await browser.close()
+})
--- specs/e2e/utils/response.ts
+++ specs/e2e/utils/response.ts
@@ -1,3 +1,8 @@
+import path from 'path'
 export const waitForResponse = (s) => (resp) => {
   return resp.url().includes(s) && [200, 201, 204].includes(resp.status())
+}
+
+export function getScreenshotPath(img: string): string {
+  return path.join(process.cwd(), 'specs', 'e2e', 'screenshots', img)
 }
