c7b5935564e50f11d1cc5a490f6c96153c05edff
Alessandro Casazza <alessandro@commercelayer.io>
2022-05-13 17:05:11 +0200

    fix: Lose order data when reload the thank you page

---
+++ src/utils/checkIncludeResource.ts
@@ -0,0 +1,26 @@
+import { ResourceIncluded } from '#reducers/OrderReducer'
+import type { Order } from '@commercelayer/sdk'
+
+type Params = {
+  order: Order
+  resourceInclude: ResourceIncluded[]
+}
+
+export default function checkIncludeResources({
+  order,
+  resourceInclude,
+}: Params) {
+  const checkKeys: boolean[] = []
+  resourceInclude.forEach((v) => {
+    const isMultiKey = v.includes('.')
+    if (isMultiKey) {
+      const [first] = v.split('.')
+      const resource = order?.[first as keyof Order]
+      if (resource === undefined) checkKeys.push(false)
+    } else {
+      const resource = order?.[v as keyof Order]
+      if (resource === undefined) checkKeys.push(false)
+    }
+  })
+  return checkKeys.length === 0
+}
