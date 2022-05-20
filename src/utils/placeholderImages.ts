10b331352f983225ba1ecc2d2a6865626a3ba05c
Alessandro Casazza <alessandro@commercelayer.io>
2022-05-20 11:56:26 +0200

    chore: Remove pages folder from eslint and mimize d.ts files

---
+++ src/utils/placeholderImages.ts
@@ -0,0 +1,4 @@
+export const defaultImgUrl =
+  'https://data.commercelayer.app/assets/images/placeholders/img_placeholder.svg'
+export const defaultGiftCardImgUrl =
+  'https://data.commercelayer.app/assets/images/placeholders/gift_placeholder.svg'
