ecc778a10b3a250df30039d559ff8985d8a26d86
Alessandro Casazza <alessandro@commercelayer.io>
2022-06-09 14:32:52 +0200

    specs: Add new specs

---
+++ specs/e2e/order/add-item-to-hosted-cart.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '../baseFixtures'
+import { OrderPage } from '../models'
+
+test.describe('Add item to hosted cart', () => {
+  test('Default hosted cart url', async ({ page }) => {
+    const order = new OrderPage(page)
+    await order.goto('order/add-item-to-hosted-cart')
+    await order.addItemToCart('BABYONBU000000E63E7412MX')
+    await order.checkCurrentUrl()
+  })
+  test('Custom hosted cart url', async ({ page }) => {
+    const order = new OrderPage(page)
+    await order.goto('order/add-item-to-hosted-cart?hostedCartUrl=true')
+    await order.addItemToCart('BABYONBU000000E63E7412MX')
+  })
+})
