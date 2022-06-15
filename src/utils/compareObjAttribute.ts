81e9a998b7d1c23c36865a42efb26f7d1ffc52cd
Alessandro Casazza <alessandro@commercelayer.io>
2022-06-15 18:02:29 +0200

    perf: Avoid duplicate requests from external re-rendering

--- src/utils/compareObjAttribute.ts
+++ src/utils/compareObjAttribute.ts
@@ -5,6 +5,15 @@ type TArgs<A, B> = {
   object: B
 }
 
+function sortObj<T extends Item>(obj: T) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((result, key) => {
+      result[key] = obj[key]
+      return result
+    }, {} as Item)
+}
+
 export default function compareObjAttribute<A extends Item, B extends Item>({
   attributes,
   object,
@@ -13,12 +22,12 @@ export default function compareObjAttribute<A extends Item, B extends Item>({
   Object.keys(object).forEach((v) => {
     const element = attributes[v]
     const compare = object[v]
-    if (
-      typeof element === 'object' &&
-      element &&
-      JSON.stringify(element) !== JSON.stringify(compare)
-    ) {
-      returnObj[v] = element
+    if (typeof element === 'object' && element) {
+      const elementSorted = sortObj(element)
+      const compareSorted = sortObj(compare)
+      if (JSON.stringify(elementSorted) !== JSON.stringify(compareSorted)) {
+        returnObj[v] = element
+      }
     }
     if (typeof element !== 'object' && element && element !== compare) {
       returnObj[v] = element
