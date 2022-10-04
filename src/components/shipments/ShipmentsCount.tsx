04eb5b8e45bd46d5d7347b9aeb1412b06437e643
Alessandro Casazza <alessandro@commercelayer.io>
2022-10-04 17:10:36 +0200

    feat: Add new `ShipmentsCount` component. Resolve #284.

---
+++ src/components/shipments/ShipmentsCount.tsx
@@ -0,0 +1,37 @@
+import Parent from '#components/utils/Parent'
+import ShipmentContext from '#context/ShipmentContext'
+import { ChildrenFunction } from '#typings/index'
+import type { Shipment } from '@commercelayer/sdk'
+import { useContext } from 'react'
+
+interface ChildrenProps extends Omit<Props, 'children'> {
+  /**
+   * Shipments of the current order
+   */
+  shipments?: Shipment[]
+  /**
+   * Quantity of the shipments
+   */
+  quantity: number
+}
+
+interface Props extends Omit<JSX.IntrinsicElements['span'], 'children'> {
+  children?: ChildrenFunction<ChildrenProps>
+}
+
+export function ShipmentsCount({ children, ...p }: Props): JSX.Element {
+  const { shipments } = useContext(ShipmentContext)
+  const quantity = shipments?.length ?? 0
+  const childrenProps: ChildrenProps = {
+    ...p,
+    quantity,
+    shipments
+  }
+  return children ? (
+    <Parent {...childrenProps}>{children}</Parent>
+  ) : (
+    <span {...p}>{quantity}</span>
+  )
+}
+
+export default ShipmentsCount
