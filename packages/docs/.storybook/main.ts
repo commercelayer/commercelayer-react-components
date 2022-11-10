f9d1f516161d3224ec9b4a420a48d35c6f4844c5
Alessandro Casazza <alessandro@commercelayer.io>
2022-11-10 15:06:42 -0800

    chore: Add vite to storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,15 +1,17 @@
-export default {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  // core: {
-  //   builder: '@storybook/builder-vite'
-  // }
-}
+// .storybook/main.ts
+
+import type { StorybookViteConfig } from '@storybook/builder-vite';
+
+const config: StorybookViteConfig = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    // Add your configuration here
+    return config;
+  },
+};
+
+export default config;
--- packages/docs/stories/auth/CommerceLayer.stories.tsx
+++ packages/docs/stories/auth/CommerceLayer.stories.tsx
@@ -2,8 +2,6 @@ import { ComponentMeta, type ComponentStory } from '@storybook/react'
 
 import CommerceLayer from '@commercelayer/react-components/auth/CommerceLayer'
 
-// const CommerceLayer = (): JSX.Element => <>Ciao</>
-
 const Story: ComponentMeta<typeof CommerceLayer> = {
   title: 'CommerceLayer',
   component: CommerceLayer
@@ -15,4 +13,5 @@ const Template: ComponentStory<typeof CommerceLayer> = (args) => (
   <CommerceLayer {...args} />
 )
 
-export const Primary = Template.bind({ accessToken: '', endpoint: '' })
+export const Primary = Template.bind({})
+Primary.args = { accessToken: 'sdsad', endpoint: 'sdsa' }
