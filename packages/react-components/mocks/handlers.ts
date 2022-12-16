915c7e1b9d37d4f3d08e88b7ae3bd71a4859f3aa
Alessandro Casazza <alessandro@commercelayer.io>
2022-12-16 18:30:05 +0100

    specs: Get original network requests if there is not the mock one

--- packages/react-components/mocks/handlers.ts
+++ packages/react-components/mocks/handlers.ts
@@ -11,5 +11,60 @@ export const handlers = [
   }),
   rest.get(`${baseUrl}/customer_addresses`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(customerAddresses))
+  }),
+  rest.get(`${baseUrl}/prices*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.get(`${baseUrl}/skus*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.get(`${baseUrl}/sku_options*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.post(`${baseUrl}/orders*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.get(`${baseUrl}/orders*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.post(`${baseUrl}/line_items*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.get(`${baseUrl}/line_items*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.patch(`${baseUrl}/line_items*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.delete(`${baseUrl}/line_items*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.post(`${baseUrl}/line_item_options*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
+  }),
+  rest.get(`${baseUrl}/line_item_options*`, async (req, res, ctx) => {
+    const originalResponse = await ctx.fetch(req)
+    const originalResData = await originalResponse.json()
+    return await res(ctx.status(200), ctx.json(originalResData))
   })
 ]
