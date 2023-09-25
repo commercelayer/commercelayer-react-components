62d6ab4f3409bf630bdd63349822368d4f589e24
Alessandro Casazza <alessandro@commercelayer.io>
2023-09-25 11:05:40 +0200

    chore: Restore stashed files.

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,17 +1,37 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+import { type StorybookConfig } from '@storybook/react-vite'
+import { resolve } from 'path'
+import { mergeConfig, type UserConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+const viteOverrides: UserConfig = {
+  base: process.env.VITE_BASE_URL,
+  plugins: [tsconfigPaths({
+    projects: [resolve(__dirname, '../../react-components/tsconfig.json'), resolve(__dirname, '../tsconfig.json')]
+  })]
+}
+
+const storybookConfig: StorybookConfig = {
+  async viteFinal(config) {
+    return mergeConfig(config, viteOverrides)
+  },
+  stories: ['../stories/*.stories.mdx', '../stories/**/*.stories.@(mdx|js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  // @ts-expect-error This 'managerEntries' exists.
+  managerEntries: [require.resolve('./addon-container/manager.tsx')],
   framework: {
-    name: "@storybook/react-vite",
-    options: {},
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  core: {
+    disableTelemetry: true
+  },
+  features: {
+    storyStoreV7: true
   },
   docs: {
-    autodocs: "tag",
-  },
-};
-export default config;
+    autodocs: true,
+    docsMode: true
+  }
+}
+
+module.exports = storybookConfig
--- packages/docs/.storybook/manager.ts
+++ packages/docs/.storybook/manager.ts
@@ -1,6 +1,6 @@
-import { addons } from '@storybook/addons'
-import theme from './theme'
+import { addons } from '@storybook/manager-api'
+import commercelayerTheme from './commercelayer.theme'
 
 addons.setConfig({
-  theme
+  theme: commercelayerTheme,
 })
