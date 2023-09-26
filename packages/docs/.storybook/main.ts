7d5a9e3093b1b655497378b7ac13437fc62912f2
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-09-26 11:31:29 +0200

    docs: add getting started section

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -19,14 +19,12 @@ const storybookConfig: StorybookConfig = {
   async viteFinal(config) {
     return mergeConfig(config, viteOverrides)
   },
-  stories: [
-    '../stories/*.stories.mdx',
-    '../stories/**/*.stories.@(mdx|js|jsx|ts|tsx)'
-  ],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm'
   ],
   framework: {
     name: '@storybook/react-vite',
