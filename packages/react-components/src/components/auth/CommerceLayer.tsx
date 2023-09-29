a7b7d8e13fa5c2755fee97dba42a2d997ef00fda
Alessandro Casazza <alessandro@commercelayer.io>
2023-09-29 18:07:41 +0200

    feat: Set `endpoint` prop as optional and add `domain` prop on `CommerceLayer` component. Resolve #419.

--- packages/react-components/src/components/auth/CommerceLayer.tsx
+++ packages/react-components/src/components/auth/CommerceLayer.tsx
@@ -1,15 +1,38 @@
 import CommerceLayerContext from '#context/CommerceLayerContext'
 import ErrorBoundary from '#components/utils/ErrorBoundary'
 import type { DefaultChildrenType } from '#typings/globals'
+import jwt from '#utils/jwt'
 
 interface Props {
+  /**
+   * Accept a React node as children.
+   */
   children: DefaultChildrenType
+  /**
+   * The access token to authenticate the API calls.
+   */
   accessToken: string
-  endpoint: string
+  /**
+   * The endpoint to make the API calls. e.g. https://yourdomain.commercelayer.io
+   */
+  endpoint?: string
+  /**
+   * The domain to make the API calls. e.g. commercelayer.io
+   */
+  domain?: string
 }
 
+/**
+ * CommerceLayer component
+ */
 export function CommerceLayer(props: Props): JSX.Element {
   const { children, ...p } = props
+  if (!p.endpoint) {
+    const { organization } = jwt(p.accessToken)
+    p.endpoint = `https://${organization.slug}.${
+      p.domain ?? 'commercelayer.io'
+    }`
+  }
   return (
     <ErrorBoundary>
       <CommerceLayerContext.Provider value={{ ...p }}>
