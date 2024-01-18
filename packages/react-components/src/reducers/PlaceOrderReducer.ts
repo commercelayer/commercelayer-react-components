eabf0acbdeabc2d7f3abf46b6dd10e1283899324
Alessandro Casazza <alessandro@commercelayer.io>
2024-01-18 15:44:22 +0100

    fix: Reset save customer payment and avoid process interruption after placing the order.

--- packages/react-components/src/reducers/PlaceOrderReducer.ts
+++ packages/react-components/src/reducers/PlaceOrderReducer.ts
@@ -255,7 +255,8 @@ export async function setPlaceOrder({
       }
       switch (paymentType) {
         case 'braintree_payments': {
-          if (saveToWallet()) {
+          const total = order?.total_amount_cents ?? 0
+          if (saveToWallet() && total > 0) {
             await sdk.orders.update({
               id: order.id,
               _save_payment_source_to_customer_wallet: true
@@ -275,8 +276,9 @@ export async function setPlaceOrder({
           const orderUpdated = await sdk.orders.update(updateAttributes, {
             include
           })
+          const total = orderUpdated?.total_amount_cents ?? 0
           if (setOrder) setOrder(orderUpdated)
-          if (saveToWallet()) {
+          if (saveToWallet() && total > 0) {
             sdk.orders
               .update({
                 id: order.id,
