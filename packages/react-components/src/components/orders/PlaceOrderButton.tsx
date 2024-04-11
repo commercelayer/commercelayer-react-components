e019383b3abea53592f008be8d651454cb25bd9b
Alessandro Casazza <alessandro@commercelayer.io>
2024-04-11 11:19:10 +0200

    fix(adyen-payments): Avoid the autoplace order with customer payment source reloading the page.

--- packages/react-components/src/components/orders/PlaceOrderButton.tsx
+++ packages/react-components/src/components/orders/PlaceOrderButton.tsx
@@ -202,7 +202,15 @@ export function PlaceOrderButton(props: Props): JSX.Element {
         currentCustomerPaymentSourceId == null &&
         autoPlaceOrder
       ) {
-        void handleClick()
+        // NOTE: This is a workaround for the case when the user reloads the page after selecting a customer payment source
+        if (
+          // @ts-expect-error no type
+          order?.payment_source?.payment_response?.merchantReference?.includes(
+            order?.number
+          )
+        ) {
+          void handleClick()
+        }
       }
     }
   }, [
