066d1802cb12db4921f6c37cf588adffa21c34d2
Alessandro Casazza <alessandro@commercelayer.io>
2025-03-27 10:44:47 +0100

    Add new getAccessToken function. Resolve #617

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,13 +3,10 @@ import { defineConfig } from "tsup"
 const env = process.env.NODE_ENV
 
 export default defineConfig((options) => ({
-  sourcemap: true, // source map is only available in prod
-  clean: true, // clean dist before build
-  dts: true, // generate dts file for main module
-  format: ["cjs", "esm"], // generate cjs and esm files
-  minify: !options.watch,
-  bundle: true,
-  watch: env === "development",
-  target: "es2020",
-  entry: ["src/index.ts"],
+  entryPoints: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  outDir: "dist",
+  clean: true,
+  treeshake: true,
 }))
