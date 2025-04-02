bc2a0919f12028b570e5a842c25593a0739d798c
Alessandro Casazza <alessandro@commercelayer.io>
2025-04-02 11:34:49 +0200

    Add retrievePrice function. Resolve #621

---
+++ packages/core/src/prices/retrievePrice.spec.ts
@@ -0,0 +1,27 @@
+import type { QueryFilter } from "@commercelayer/sdk"
+import { describe, expect } from "vitest"
+import { type CoreTestInterface, coreTest } from "#extender"
+import { getPrices } from "./getPrices.js"
+import { retrievePrice } from "./retrievePrice.js"
+
+describe("retrievePrice", () => {
+  coreTest<CoreTestInterface>(
+    "should return a single price",
+    async ({ accessToken }) => {
+      const token = accessToken?.accessToken
+      const firstPrice = (await getPrices({ accessToken: token })).first()
+      expect(firstPrice).toBeDefined()
+      if (!firstPrice) {
+        throw new Error("No price found")
+      }
+      const id = firstPrice?.id
+      const result = await retrievePrice({
+        id: id,
+        accessToken: token,
+      })
+      expect(result).toBeDefined()
+      expect(result.id).toBe(id)
+      expect(result.sku_code).toBe(firstPrice.sku_code)
+    },
+  )
+})
