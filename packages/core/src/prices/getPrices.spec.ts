10c1bd4e2919a9845fae9f4d47d38e9d356626f9
Alessandro Casazza <alessandro@commercelayer.io>
2025-04-22 17:37:37 +0200

    Add updatePrice function, and tests, remove package jwt-decode

--- packages/core/src/prices/getPrices.spec.ts
+++ packages/core/src/prices/getPrices.spec.ts
@@ -1,35 +1,29 @@
-import type { QueryFilter } from "@commercelayer/sdk"
+import type { Price, QueryParamsList } from "@commercelayer/sdk"
 import { describe, expect } from "vitest"
-import { type CoreTestInterface, coreTest } from "#extender"
+import { coreTest } from "#extender"
 import { getPrices } from "./getPrices.js"
 
 describe("getPrices", () => {
-  coreTest<CoreTestInterface>(
-    "should return a list of prices",
-    async ({ accessToken }) => {
-      const token = accessToken?.accessToken
-      const result = await getPrices({ accessToken: token })
-      expect(result).toBeDefined()
-    },
-  )
+  coreTest("should return a list of prices", async ({ accessToken }) => {
+    const token = accessToken?.accessToken
+    const result = await getPrices({ accessToken: token })
+    expect(result).toBeDefined()
+  })
 
-  coreTest<CoreTestInterface>(
-    "should return a single price",
-    async ({ accessToken }) => {
-      const token = accessToken?.accessToken
-      const params: QueryFilter = {
-        filters: {
-          sku_code_eq: "DIGITALPRODUCT",
-        },
-      }
-      // Call the getPrices function
-      const result = await getPrices({ accessToken: token, ...params })
-      // Assert the expected result
-      expect(result).toBeDefined()
-      expect(result.getRecordCount()).toBe(1)
-      // Add more assertions based on the expected behavior of the getPrices function
-    },
-  )
+  coreTest("should return a single price", async ({ accessToken }) => {
+    const token = accessToken?.accessToken
+    const params = {
+      filters: {
+        sku_code_eq: "DIGITALPRODUCT",
+      },
+    } satisfies QueryParamsList<Price>
+    // Call the getPrices function
+    const result = await getPrices({ accessToken: token, params })
+    // Assert the expected result
+    expect(result).toBeDefined()
+    expect(result.getRecordCount()).toBe(1)
+    // Add more assertions based on the expected behavior of the getPrices function
+  })
 
   // Add more test cases for different scenarios
 })
--- packages/core/src/types/base.ts
+++ packages/core/src/types/base.ts
@@ -1,3 +1,8 @@
+import type { ResourcesConfig } from "@commercelayer/sdk"
+
 export interface RequestConfig {
   accessToken: string
+  id?: string
+  params?: unknown
+  options?: ResourcesConfig
 }
