c19b84f0d59627250618df04da5c47ae8cc650b2
Alessandro Casazza <alessandro@commercelayer.io>
2025-04-22 17:37:37 +0200

    Add updatePrice function, and tests, remove package jwt-decode

--- packages/core/extender.ts
+++ packages/core/extender.ts
@@ -2,6 +2,8 @@ import { test } from "vitest"
 import { getAccessToken } from "./src/auth/getAccessToken.js"
 
 const clientId = import.meta.env.VITE_SALES_CHANNEL_CLIENT_ID
+const integrationClientId = import.meta.env.VITE_INTEGRATION_CLIENT_ID
+const integrationClientSecret = import.meta.env.VITE_INTEGRATION_CLIENT_SECRET
 const scope = import.meta.env.VITE_SALES_CHANNEL_SCOPE
 const domain = import.meta.env.VITE_DOMAIN
 let accessToken: ReturnType<typeof getAccessToken>
@@ -10,12 +12,15 @@ export interface CoreTestInterface {
   accessToken: Awaited<ReturnType<typeof getAccessToken>>
   config: {
     clientId: string
-    scope: string
+    scope?: string
     domain: string
   }
 }
 
-export const coreTest = test.extend({
+/**
+ * This test is used to run integration tests with the sales channel client.
+ */
+export const coreTest = test.extend<CoreTestInterface>({
   // biome-ignore lint/correctness/noEmptyPattern: need to object destructure as the first argument
   accessToken: async ({}, use) => {
     if (accessToken == null) {
@@ -34,6 +39,31 @@ export const coreTest = test.extend({
   config: {
     clientId,
     scope,
+    domain,
+  },
+})
+
+/**
+ * This test is used to run integration tests with the integration client.
+ */
+export const coreIntegrationTest = test.extend<CoreTestInterface>({
+  // biome-ignore lint/correctness/noEmptyPattern: need to object destructure as the first argument
+  accessToken: async ({}, use) => {
+    if (accessToken == null) {
+      accessToken = await getAccessToken({
+        grantType: "client_credentials",
+        config: {
+          clientId: integrationClientId,
+          clientSecret: integrationClientSecret,
+          domain,
+        },
+      })
+    }
+    use(accessToken)
+    accessToken = undefined
+  },
+  config: {
+    clientId: integrationClientId,
     domain,
   },
 })
