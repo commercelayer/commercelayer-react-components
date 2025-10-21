835009f9c6dadcd58c71cde558ae2e7e315ed4e8
Alessandro Casazza <alessandro@commercelayer.io>
2025-10-21 18:08:01 +0200

    Fix ship to different address mode using customer address

--- packages/react-components/src/reducers/AddressReducer.ts
+++ packages/react-components/src/reducers/AddressReducer.ts
@@ -276,8 +276,8 @@ export async function saveAddresses({
             )
           }
         } else if (!shipToDifferentAddress && billingAddressCloneId) {
-          delete orderAttributes._shipping_address_clone_id
-          orderAttributes._shipping_address_same_as_billing = true
+          delete orderAttributes._shipping_address_same_as_billing
+          orderAttributes._shipping_address_clone_id = billingAddressCloneId
         }
         if (!shipToDifferentAddress && billingAddressCloneId) {
           orderAttributes._shipping_address_same_as_billing = true
@@ -319,6 +319,7 @@ export async function saveAddresses({
           id: order.id,
           attributes: orderAttributes,
         })
+        console.log("Order updated:", orderUpdated)
         return { success: true, order: orderUpdated?.order }
       }
     }
