aea671658f6f5f1e1252c9cd56f636bc7e8afb2d
Alessandro Casazza <alessandro@commercelayer.io>
2026-03-18 16:11:15 +0100

    ✨ feat(PrivacyAndTermsCheckbox): fallback to org config for privacy_url and terms_url
    Closes #721
    Ref: https://github.com/commercelayer/mfe-checkout/issues/611

--- packages/react-components/src/components/orders/PrivacyAndTermsCheckbox.tsx
+++ packages/react-components/src/components/orders/PrivacyAndTermsCheckbox.tsx
@@ -1,16 +1,24 @@
+import CommerceLayerContext from '#context/CommerceLayerContext'
 import OrderContext from '#context/OrderContext'
 import PlaceOrderContext from '#context/PlaceOrderContext'
+import { useOrganizationConfig } from '#utils/organization'
 import { useContext, useEffect, useState, type JSX } from 'react';
 import BaseInput, { type BaseInputProps } from '../utils/BaseInput'
 
 export function PrivacyAndTermsCheckbox(
   props: Partial<BaseInputProps>
 ): JSX.Element {
+  const { accessToken, endpoint } = useContext(CommerceLayerContext)
   const { order } = useContext(OrderContext)
   const { placeOrderPermitted } = useContext(PlaceOrderContext)
   const [forceDisabled, setForceDisabled] = useState(true)
   const [checked, setChecked] = useState(false)
   const fieldName = 'privacy-terms'
+  const organizationConfig = useOrganizationConfig({ accessToken, endpoint })
+
+  const privacyUrl = order?.privacy_url ?? organizationConfig?.urls?.privacy
+  const termsUrl = order?.terms_url ?? organizationConfig?.urls?.terms
+
   const handleChange: any = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const v = e.target?.checked
     setChecked(v)
@@ -18,13 +26,13 @@ export function PrivacyAndTermsCheckbox(
     if (placeOrderPermitted) placeOrderPermitted()
   }
   useEffect(() => {
-    if (order?.privacy_url && order?.terms_url) setForceDisabled(false)
+    if (privacyUrl && termsUrl) setForceDisabled(false)
     if (!checked) localStorage.setItem(fieldName, checked.toString())
     return () => {
       setForceDisabled(true)
       localStorage.removeItem(fieldName)
     }
-  }, [order?.privacy_url, order?.terms_url])
+  }, [privacyUrl, termsUrl])
   return (
     <BaseInput
       type='checkbox'
