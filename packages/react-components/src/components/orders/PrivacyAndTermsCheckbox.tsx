d278ebeeab2a88df58f39e6cf6f48de9b653e9bb
Alessandro Casazza <alessandro@commercelayer.io>
2026-03-18 16:32:58 +0100

    🔧 fix(PrivacyAndTermsCheckbox): fix lint errors

--- packages/react-components/src/components/orders/PrivacyAndTermsCheckbox.tsx
+++ packages/react-components/src/components/orders/PrivacyAndTermsCheckbox.tsx
@@ -1,25 +1,25 @@
-import CommerceLayerContext from '#context/CommerceLayerContext'
-import OrderContext from '#context/OrderContext'
-import PlaceOrderContext from '#context/PlaceOrderContext'
-import { useOrganizationConfig } from '#utils/organization'
-import { useContext, useEffect, useState, type JSX } from 'react';
-import BaseInput, { type BaseInputProps } from '../utils/BaseInput'
+import { type JSX, useContext, useEffect, useState } from "react"
+import CommerceLayerContext from "#context/CommerceLayerContext"
+import OrderContext from "#context/OrderContext"
+import PlaceOrderContext from "#context/PlaceOrderContext"
+import { useOrganizationConfig } from "#utils/organization"
+import BaseInput, { type BaseInputProps } from "../utils/BaseInput"
 
 export function PrivacyAndTermsCheckbox(
-  props: Partial<BaseInputProps>
+  props: Partial<BaseInputProps>,
 ): JSX.Element {
   const { accessToken, endpoint } = useContext(CommerceLayerContext)
   const { order } = useContext(OrderContext)
   const { placeOrderPermitted } = useContext(PlaceOrderContext)
   const [forceDisabled, setForceDisabled] = useState(true)
   const [checked, setChecked] = useState(false)
-  const fieldName = 'privacy-terms'
+  const fieldName = "privacy-terms"
   const organizationConfig = useOrganizationConfig({ accessToken, endpoint })
 
   const privacyUrl = order?.privacy_url ?? organizationConfig?.urls?.privacy
   const termsUrl = order?.terms_url ?? organizationConfig?.urls?.terms
 
-  const handleChange: any = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const v = e.target?.checked
     setChecked(v)
     localStorage.setItem(fieldName, v.toString())
@@ -32,10 +32,10 @@ export function PrivacyAndTermsCheckbox(
       setForceDisabled(true)
       localStorage.removeItem(fieldName)
     }
-  }, [privacyUrl, termsUrl])
+  }, [privacyUrl, termsUrl, checked])
   return (
     <BaseInput
-      type='checkbox'
+      type="checkbox"
       name={fieldName}
       disabled={forceDisabled}
       onChange={handleChange}
